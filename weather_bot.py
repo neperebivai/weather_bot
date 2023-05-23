6ad8007b79f9386b29233e138720a5945148026b
neperebivai <133754117+neperebivai@users.noreply.github.com>
2023-05-23 19:45:53 +0300

    Update weather_bot.py
    exception in get_weather

--- weather_bot.py
+++ weather_bot.py
@@ -3,13 +3,16 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
 
-bot = Bot(token='token')
+BOT_TOKEN = 'bot_token'
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands='start')
 async def greeting_message(message):
-    msg = f'Приветствую Вас, {message.from_user.first_name}. Я могу подсказать какая сейчас погода на улице. Для этого выберите интересующий Вас город:'
+    msg = f'Приветствую Вас, {message.from_user.first_name}.\n' \
+          f'Я могу подсказать какая сейчас погода на улице.\n' \
+          f'Для этого выберите интересующий Вас город:'
     kb = [
         [
             types.KeyboardButton(text="Тверь"),
@@ -21,8 +24,8 @@ async def greeting_message(message):
         resize_keyboard=True,
         input_field_placeholder='или введите название вручную'
     )
-
     await message.answer(msg, reply_markup=keyboard)
+
 
 @dp.message_handler(content_types='text')
 async def send_weather(message: types.Message):
@@ -30,14 +33,19 @@ async def send_weather(message: types.Message):
     weather = get_weather(city)
     await message.answer(weather)
 
+
 def get_weather(city):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=your_id&units=metric'
-    response = requests.get(url)
-    data = response.json()
-    weather_description = data['weather'][0]['description']
-    temperature = data['main']['temp']
-    return f'Погода в городе {city}: {weather_description}, температура: {temperature}°C'
+    try:
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&appid=your_id&units=metric'
+        response = requests.get(url)
+        data = response.json()
+        weather_description = data['weather'][0]['description']
+        temperature = data['main']['temp']
+        return f'Погода в городе {city}: {weather_description}, температура: {temperature}°C'
+    except:
+        return f'Название города не распознано.' \
+               f'Прверьте название и попробуйте ещн раз.'
 
 
-
-executor.start_polling(dp, skip_updates=True)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
