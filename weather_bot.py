d98ace6c30dee96298ce9536856654a4eb5e44f5
Neperebivai <neperebivai@gmail.com>
2023-05-27 03:33:39 +0300

    reformat message

--- weather_bot.py
+++ weather_bot.py
@@ -12,7 +12,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands='start')
 async def greeting_message(message):
     msg = f'Приветствую Вас, {message.from_user.first_name}.\n' \
-          f'Я могу подсказать какая сейчас погода на улице.\n' \
+          f'Я могу подсказать прогнозируемую погоду.\n' \
           f'Для этого выберите интересующий Вас город:'
     kb = [
         [
@@ -36,21 +36,36 @@ async def send_weather(message: types.Message):
 
 
 def get_weather(city):
+    # Создаём ответное сообщение
     try:
         url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&lang=ru&appid={WEATHER_TOKEN}&units=metric'
         response = requests.get(url)
         data = response.json()
-        forecast = f'Прогноз для города {city}:\n'
-        for time_point in data['list']:
-            point = time_point['dt_txt'], '{0:+3.0f}'.format(time_point['main']['temp']), time_point['weather'][0]['description']
-            point = str(point).replace('\'', '')
-            point = point.strip('(' ')')
-            forecast = forecast + point + '\n'
+        forecast = f'Прогноз погоды для города {city}:\n {generate_message(data)}'
         return forecast
-
     except:
         return f'Название города не распознано.' \
-               f'Прверьте название и попробуйте ещн раз.'
+            f'Прверьте название и попробуйте ещн раз.'
+
+
+def generate_message(data):
+    # Генерируем блок с прогнозом погоды
+    message = ''
+    for time_point in data['list']:
+        time = str(time_point['dt_txt'])
+        point = get_time(time), '{0:+3.0f}'.format(time_point['main']['temp']), time_point['weather'][0][
+            'description']
+        point = str(point).replace('\'', '')
+        point = point.strip('(' ')')
+        message = message + point + '\n'
+    return message
+
+
+def get_time(time):
+    # Компактное отображение даты
+    time_format = '%Y-%m-%d %H:%M:%S'
+    sep_time = datetime.strptime(time, time_format)
+    return str(sep_time.strftime('%d%b %H:%M'))
 
 
 if __name__ == '__main__':
